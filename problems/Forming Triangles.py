e53e522a7f271cd967d94d2d7cb770196724a195
Anas Badr <anasihabezmc@gmail.com>
2024-02-17 17:29:37 +0200

    Added solution for 1922B

--- problems/Forming Triangles.py
+++ problems/Forming Triangles.py
@@ -1,26 +1,27 @@
 num_cases = int(input())
 
-
-def valid_triangle(x, y, z):
-    return x + y > z and x + z > y and y + z > x
-
-
 for _ in range(num_cases):
     string_len, string = [input().split() if i == 1 else int(input()) for i in range(2)]
     ans = 0
-    if string_len == 1:
+    if string_len < 3:
         print(0)
         continue
-    num_lengths = {i: 0 for i in string}
+
+    num_lengths = {int(i): 0 for i in string}
     for i in range(string_len):
-        num_lengths[string[i]] += 1
+        num_lengths[int(string[i])] += 1
+
+    num_lengths = dict(sorted(num_lengths.items()))
+    sum = 0
 
     for length, count in num_lengths.items():
         if count >= 3:
             ans += count * (count - 1) * (count - 2) // 6
 
         if count >= 2:
-            ans += count * (count - 1) // 2 * (string_len - count)
+            ans += count * (count - 1) // 2 * sum
+
+        sum += count
 
     print(ans)
 
